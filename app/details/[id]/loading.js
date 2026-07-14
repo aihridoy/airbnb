8a6e46c68afdedf8a98074e0f673304d3676aaf0
aihridoy <cse_1912020168@lus.ac.bd>
2026-07-15 02:23:11 +0600

    feat: wire details loading.js to real DetailsPageSkeleton

--- app/details/[id]/loading.js
+++ app/details/[id]/loading.js
@@ -1,7 +1,5 @@
+import DetailsPageSkeleton from "@/components/skeletons/DetailsPageSkeleton";
+
 export default function Loading() {
-  return (
-    <div className="flex items-center justify-center min-h-[50vh]">
-      <div className="h-10 w-10 animate-spin rounded-full border-4 border-gray-200 border-t-primary" />
-    </div>
-  );
+  return <DetailsPageSkeleton />;
 }
